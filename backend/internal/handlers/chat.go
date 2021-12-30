cb021f92470fe6ccf1dd8ae1f1cf7230b7295798
darmiel <71837281+darmiel@users.noreply.github.com>
2021-12-30 22:05:21 +0100

    feat[backend]: check chat message length

--- backend/internal/handlers/chat.go
+++ backend/internal/handlers/chat.go
@@ -1,12 +1,19 @@
 package handlers
 
 import (
+	"errors"
 	"github.com/Qwiri/GYF/backend/pkg/model"
 	"github.com/gofiber/websocket/v2"
 	"strings"
 )
 
+var ErrMessageEmpty = errors.New("message empty")
+
 func handleChat(_ *websocket.Conn, game *model.Game, client *model.Client, _ string, message []string) error {
-	game.Broadcast(model.NewResponse("CHAT", client.Name, strings.Join(message, " ")))
+	msg := strings.TrimSpace(strings.Join(message, " "))
+	if msg == "" {
+		return ErrMessageEmpty
+	}
+	game.Broadcast(model.NewResponse("CHAT", client.Name, msg))
 	return nil
 }
