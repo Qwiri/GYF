f5024c8e3dbe73d0801fa3f303eed8955edf2006
darmiel <71837281+darmiel@users.noreply.github.com>
2021-12-30 21:43:53 +0100

    refactor[backend]: add leader for LIST

--- backend/internal/handlers/list.go
+++ backend/internal/handlers/list.go
@@ -6,11 +6,15 @@ import (
 )
 
 func handleList(conn *websocket.Conn, game *model.Game, _ *model.Client, _ string, _ []string) error {
+	type listObj struct {
+		Name   string `json:"name"`
+		Leader bool   `json:"leader"`
+	}
 	// collect client names
 	clientNames := make([]interface{}, len(game.Clients))
 	var i = 0
 	for _, c := range game.Clients {
-		clientNames[i] = c.Name
+		clientNames[i] = listObj{c.Name, c.Leader}
 		i += 1
 	}
 	model.NewResponse("LIST", clientNames...).Respond(conn)
