786478056c1801d336d6a7a2648a841c17565034
Tom Romeo <Tom.Romeo.Doil@gmail.com>
2021-12-31 14:18:52 +0100

    feat[frontend]: gif submission input and display

--- frontend/src/screens/DisplayTopic.svelte
+++ frontend/src/screens/DisplayTopic.svelte
@@ -1,9 +1,21 @@
 <script lang="ts">
+import { toast } from "@zerodevx/svelte-toast";
+
     import { leader, round, ws } from "../store";
 
     const sendSkip = (_) => {
         $ws.send("SKIP");
     };
+
+    const sendGif = (_) => {
+        if (!gifBuffer) {
+            toast.push("You need to supply a gif url");
+            return;
+        }
+        $ws.send(`SUBMIT_GIF ${gifBuffer}`);
+    };
+
+    let gifBuffer: string;
 </script>
 
 <!-- Display Round Number -->
@@ -13,6 +25,11 @@
 
 <!-- Display Topic -->
 <h2>{$round.topic}</h2>
+<input type="text" placeholder="Enter a gif url" bind:value="{gifBuffer}"/>
+<button on:click="{sendGif}">Submit!</button>
+{#if gifBuffer}
+    <img width="200px" src="{gifBuffer}" alt="submitted gif" />
+{/if}
 
 <!-- Skip Button for Leader -->
 {#if $leader}
