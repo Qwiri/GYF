65b0c35c8fbc5d1d7756d5a2d6842ab9f3d6215b
Tom Romeo <Tom.Romeo.Doil@gmail.com>
2022-01-01 18:39:26 +0100

    refactor[frontend]: vote view

--- frontend/src/screens/game/Voting.svelte
+++ frontend/src/screens/game/Voting.svelte
@@ -9,7 +9,8 @@
 <div id="submissionsWrapper">
     {#each $submissions as submission}
         <div class="image">
-            <img src="{submission}" alt="">
+            <img class="img" src="{submission}" alt="">
+            <div class="background"></div>
             <div data-url="{submission}" on:click="{submitVote}" class="overlay"></div>
         </div>
 
@@ -18,25 +19,47 @@
 
 <style lang="scss">
     #submissionsWrapper {
-        display: grid;
+        display: flex;
+        flex-wrap: wrap;
+        gap: 2rem;
+        justify-items: center;
+        justify-content: center;
 
         .overlay {
             bottom: 0;
             left: 0;
-            opacity: 0.5;
             position: absolute;
             right:0;
             top:0;
-            z-index: 1;
+            z-index: 2;
 
             &:hover {
-                background-color: #181818b6
+                box-shadow: inset 0px 0px 10px 1000px rgb(173 255 47 / 50%);
+                backdrop-filter: grayscale(1);
             }
         }
 
+        .img {
+            width: clamp(200px, 20vw, 400px);
+            z-index: 1;
+        }
+        .background {
+            position: absolute;
+            top: 0;
+            bottom: 0;
+            left: 0;
+            right: 0;
+            background-color: #131313;
+            border-radius: 0.5rem;
+            z-index: -1;
+        }
         .image{
             position: relative;
-            width: min-content;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            width: clamp(200px, 20vw, 400px);
+            height: clamp(200px, 20vw, 400px);
         }
     }
 </style>
