90d576cadb2db69e3c38fd3d7fd3d0a39df85bc3
Tom Romeo <Tom.Romeo.Doil@gmail.com>
2022-01-03 08:25:55 +0100

    feat[frontend]: gif search window skeleton

--- frontend/src/screens/game/DisplayTopic.svelte
+++ frontend/src/screens/game/DisplayTopic.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { toast } from "@zerodevx/svelte-toast";
+import { onMount } from "svelte";
     import TopicDisplay from "../../assets/TopicDisplay.svelte";
     import { leader, players, round, waitingFor, ws } from "../../store";
 
@@ -16,6 +17,9 @@
     };
 
     let gifBuffer: string;
+
+    let showGifWindow: boolean = false;
+
 </script>
 
 <!-- Display Round Number -->
@@ -27,7 +31,12 @@
     </p>
 {/if}
 
-<input type="text" placeholder="Enter a gif url" bind:value={gifBuffer} />
+<input type="text" placeholder="Enter a gif url" on:click={_ => showGifWindow = !showGifWindow} bind:value={gifBuffer} />
+<div id="searchBarWrapper">
+    {#if showGifWindow}
+        <div id="gifSearchWrapper"></div>
+    {/if}
+</div>
 <button on:click={sendGif}>Submit!</button>
 
 <!-- Skip Button for Leader -->
@@ -38,5 +47,17 @@
 <style lang="scss">
     button:hover {
         cursor: pointer;
+    }
+
+    #gifSearchWrapper {
+        width: 50vw;
+        height: 10vh;
+        position: absolute;
+        top: 0;
+        left: 0;
+        background-color: #131313;
+    }
+    #searchBarWrapper {
+        position: relative;
     }
 </style>
