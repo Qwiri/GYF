38bc5bab0be1a870aea2f6e92d001ed1f4a7d7ea
darmiel <71837281+darmiel@users.noreply.github.com>
2022-01-09 19:18:26 +0100

    feat[frontend]: highlight self (#40)

---
+++ frontend/src/assets/GameEndUser.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import { username } from "../store";
+
+    import Avatar from "./Avatar.svelte";
+
+    export let name;
+    export let height = "auto";
+</script>
+
+<div class="avatar">
+    <Avatar user={name} width="auto" {height} />
+    <span class:self={name === $username}>{name}</span>
+</div>
+
+<style lang="scss">
+    .self {
+        color: greenyellow;
+    }
+    .avatar {
+        height: 5rem;
+        width: 5rem;
+        overflow: hidden;
+        display: flex;
+        flex-direction: column;
+    }
+</style>
